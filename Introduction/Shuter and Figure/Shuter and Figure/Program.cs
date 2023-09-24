560a1fc07cd488b073a73b0f5bcc5f40c2ea0499
user <user@WIN-VAVPHUEFSF2>
2023-09-24 11:00:50 +0700

    C#

---
+++ Introduction/Shuter and Figure/Shuter and Figure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuter_and_Figure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
